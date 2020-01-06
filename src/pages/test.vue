fd4a9b15ef03f790c74e210a70e40ff48982983c
AngryBirdsBomb <565843839@qq.com>
2020-01-07 02:41:17 +0800

    测试流星

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -2,8 +2,18 @@
     <div class="test">
         <div ref="top" class="top"></div>
         <div class="content">
-            <div class="item" v-for="(item,index) in list" :key="item.id" :style="item.circleStyle">
-                <span>{{item.circle}}</span>
+            <div
+                class="item"
+                v-for="(item, index) in list"
+                :key="item.id"
+                :style="item.circleStyle"
+            >
+                <span>{{ item.circle }}</span>
+            </div>
+        </div>
+        <div class="index">
+            <div id="stars">
+                <div class="star" style="top: 0px;left: 500px;"></div>
             </div>
         </div>
     </div>
@@ -98,42 +108,45 @@ export default {
         init() {
             let arr = JSON.parse(JSON.stringify(this.list));
             for (var i = 0; i < arr.length; i++) {
-                let item = this.createStar(600 - 100, 0 + 100,50);
+                let item = this.createStar(600 - 100, 0 + 100, 50);
                 arr[i].circle = item;
             }
             //console.log(arr,'@');
-            for(var k=0;k<arr.length;k++){
-                this.init2(arr[k],arr);
+            for (var k = 0; k < arr.length; k++) {
+                this.init2(arr[k], arr);
             }
-            
         },
-        init2(obj,arr) {
+        init2(obj, arr) {
             //console.log(arr,'#');
-            let isOk=0;
+            let isOk = 0;
             for (var j = 0; j < arr.length; j++) {
-                if(obj.id==arr[j].id){
+                if (obj.id == arr[j].id) {
                     continue;
-                }
-                else{
+                } else {
                     if (this.jiance(arr[j], obj)) {
-                    arr[j].circle = this.createStar(600 - 100, 0 + 100,50);
+                        arr[j].circle = this.createStar(600 - 100, 0 + 100, 50);
                         //this.init2(arr);
                         isOk++;
                         continue;
                     }
                 }
             }
-            if(isOk=0){
+            if ((isOk = 0)) {
                 //console.log(arr,'!');
-                for(var n=0;n<arr.length;n++){
-                    arr[n].circleStyle={'width':arr[n].circle.r*2+'px','height':arr[n].circle.r*2+'px','backgroundColor':this.randomColor(),'left':arr[n].circle.x+'px','top':arr[n].circle.y+'px'};
+                for (var n = 0; n < arr.length; n++) {
+                    arr[n].circleStyle = {
+                        width: arr[n].circle.r * 2 + "px",
+                        height: arr[n].circle.r * 2 + "px",
+                        backgroundColor: this.randomColor(),
+                        left: arr[n].circle.x + "px",
+                        top: arr[n].circle.y + "px"
+                    };
                 }
+            } else {
+                this.init2(obj, arr);
             }
-            else{
-                this.init2(obj,arr);
-            }
-            
-            this.list=arr;
+
+            this.list = arr;
             console.log(this.list);
         },
         createStar(m, n, r) {
@@ -179,34 +192,150 @@ export default {
             } else {
                 return false;
             }
+        },
+        cc(x, y) {
+            /* http://www.jq22.com/webqd5115?from=groupmessage */
+            var stars = document.getElementById("stars");
+            let _self = this;
+            // js随机生成流星
+            for (var j = 0; j < 20; j++) {
+                var newStar = document.createElement("div");
+                newStar.className = "star";
+                let top = this.randomDistance(500, -100);
+                let left = this.randomDistance(1980, 300);
+                newStar.style.top = top + "px";
+                newStar.style.left = left + "px";
+                stars.appendChild(newStar);
+            }
+
+            var star = document.getElementsByClassName("star");
+
+            // 给流星添加动画延时
+            for (var i = 0, len = star.length; i < len; i++) {
+                let disX = parseInt(star[i].offsetLeft) - x;
+                let disY = parseInt(star[i].offsetTop) - y;
+                let dis = Math.sqrt(Math.pow(disX, 2) + Math.pow(disY, 2));
+                let top = parseInt(star[i].offsetTop) ;
+                let left = parseInt(star[i].offsetLeft);
+                let timer = setInterval(() => {
+                    top--;
+                    left--;
+                    if(left<=0){
+                        left=0;
+                    }
+                    if(top<=0){
+                        top=0;
+                    }
+                    star[i].style.top = top + "px";
+                    star[i].style.left = left + "px";
+                    if(left==0&&top==0){
+                        clearInterval(timer);
+                    }
+                }, 1000);
+                star[i].style.animationDelay =
+                    i % 6 == 0 ? "0s" : i * 0.8 + "s";
+            }
+        },
+        // 封装随机数方法
+        randomDistance(max, min) {
+            var distance = Math.floor(Math.random() * (max - min + 1) + min);
+            return distance;
         }
     },
     mounted() {
-        this.init();
+        //this.init();
+        this.cc(100, 100);
     }
 };
 </script>
 <style lang="scss">
 .test {
     height: 100%;
-    
-    .content{
+
+    .content {
         position: relative;
         width: 600px;
         height: 600px;
         background-color: #000;
         margin: 0 auto;
-        .item{
+        .item {
             position: absolute;
             border-radius: 50%;
             overflow: hidden;
             text-align: center;
             word-break: break-all;
-            span{
-                display:block;
+            span {
+                display: block;
                 padding-top: 50px;
             }
         }
+    }
+}
+.index {
+    position: fixed;
+    width: 100%;
+    background: #000000;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 2;
+}
+#stars {
+    width: 100%;
+    position: relative;
+    height: 100%;
+}
+.star {
+    position: absolute;
+    display: block;
+    width: 5px;
+    height: 5px;
+    border-radius: 50%;
+    background: #fff;
+    position: absolute;
+    transform-origin: 100% 0;
+    animation: star-ani 4s linear infinite;
+    -webkit-animation: star-ani 5s linear infinite;
+    box-shadow: 0 0 5px 5px rgba(255, 255, 255, 0.3);
+    opacity: 0;
+    z-index: 2;
+}
+.star:after {
+    content: "";
+    display: block;
+    top: 0px;
+    left: 4px;
+    border: 0px solid #fff;
+    border-width: 0px 90px 2px 90px;
+    border-color: transparent transparent transparent rgba(255, 255, 255, 0.3);
+    transform: rotate(-45deg) translate3d(1px, 3px, 0);
+    box-shadow: 0 0 1px 0 rgba(255, 255, 255, 0.1);
+    transform-origin: 0% 100%;
+}
+@keyframes star-ani {
+    0% {
+        opacity: 0;
+        transform: scale(0) /* translate3d(0, 0, 0) */;
+    }
+    20% {
+        opacity: 0.8;
+        transform: scale(0.2) /* translate3d(-100px, 100px, 0) */;
+    }
+    40% {
+        opacity: 0.8;
+        transform: scale(0.4) /* translate3d(-200px, 200px, 0) */;
+    }
+    60% {
+        opacity: 0.8;
+        transform: scale(0.6) /* translate3d(-300px, 300px, 0) */;
+    }
+    80% {
+        opacity: 1;
+        transform: scale(1) /* translate3d(-350px, 350px, 0) */;
+    }
+    100% {
+        opacity: 1;
+        transform: scale(1.2) /* translate3d(-400px, 380px, 0) */;
     }
 }
 </style>
